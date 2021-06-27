b3f63eba85eea7cb386f8584d2a61b7b6ef999a7
lemon37564 <stonydragon0701@gmail.com>
2021-06-27 19:14:16 +0800

    make sounds as a class, fix bug (music plays twice)

---
+++ sounds/__init__.py
@@ -0,0 +1,11 @@
+import pygame.mixer
+
+pygame.mixer.init()
+
+import sounds.bgm
+import sounds.dead
+import sounds.shoot
+
+
+LOOP_FOREVER = -1
+LOOP_ONCE = 0
---
+++ sounds/dead.py
@@ -0,0 +1,9 @@
+import pygame.mixer
+
+__deadplayer = pygame.mixer.Sound("bgm/gameover.ogg")
+
+def play(loop):
+    __deadplayer.play(loops=loop)
+
+def stop():
+    __deadplayer.stop()
