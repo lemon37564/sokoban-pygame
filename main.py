ca11c0d5429ee307d64e7a43a2dca0bbf5ccd597
lemon37564 <stonydragon0701@gmail.com>
2021-09-06 21:03:40 +0800

    formatting main.py by using autopep8

--- main.py
+++ main.py
@@ -1,133 +1,140 @@
-import pygame 
+import pygame
 
-pygame.init() 
+pygame.init()
 pygame.font.init()
 
 from game import Game
-# initializing the constructor 
+# initializing the constructor
 
-  
-# screen resolution 
-#res = (720,720) 
-  
-# opens up a window 
+
+# screen resolution
+#res = (720,720)
+
+# opens up a window
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-  
-# white color 
-color = (255,255,255) 
-  
-# light shade of the button 
-color_light = (170,170,170) 
-  
-# dark shade of the button 
-color_dark = (100,100,100) 
-  
-# stores the width of the 
-# screen into a variable 
-width = screen.get_width() 
-  
-# stores the height of the 
-# screen into a variable 
-height = screen.get_height() 
-  
+
+# white color
+color = (255, 255, 255)
+
+# light shade of the button
+color_light = (170, 170, 170)
+
+# dark shade of the button
+color_dark = (100, 100, 100)
+
+# stores the width of the
+# screen into a variable
+width = screen.get_width()
+
+# stores the height of the
+# screen into a variable
+height = screen.get_height()
+
 # defining fonts
-smallfont = pygame.font.SysFont('Corbel',35) 
-bigfont = pygame.font.SysFont('Corbel',50) 
-  
-# rendering a text written in 
-# this font 
-text_quit = smallfont.render('quit' , True , color) 
-text_start=smallfont.render('start game' , True , color) 
-text_help=smallfont.render('tutorial' , True , color) 
-text_title=bigfont.render('Sokoban Stealer',True,color)
+smallfont = pygame.font.SysFont('Corbel', 35)
+bigfont = pygame.font.SysFont('Corbel', 50)
 
-#where BTNs and texts at
+# rendering a text written in
+# this font
+text_quit = smallfont.render('quit', True, color)
+text_start = smallfont.render('start game', True, color)
+text_help = smallfont.render('tutorial', True, color)
+text_title = bigfont.render('Sokoban Stealer', True, color)
+
+# where BTNs and texts at
 x_title = width/2-200
-x_start_btn=  width/2-150
-x_quit_btn=  width/2-150
-x_tut_btn=  width/2-150
-y_title=  height/3-70
-y_start_btn=height/3+20
-y_tut_btn=  height/3+70
-y_quit_btn= height/3+120
+x_start_btn = width/2-150
+x_quit_btn = width/2-150
+x_tut_btn = width/2-150
+y_title = height/3-70
+y_start_btn = height/3+20
+y_tut_btn = height/3+70
+y_quit_btn = height/3+120
 
-width_start_btn=200
-width_quit_btn=140
-width_tut_btn=180
-width_title=400
+width_start_btn = 200
+width_quit_btn = 140
+width_tut_btn = 180
+width_title = 400
 
-height_start_btn=40
-height_quit_btn=40
-height_tut_btn=40
-height_title=40
+height_start_btn = 40
+height_quit_btn = 40
+height_tut_btn = 40
+height_title = 40
 
-#game settings
-level_selected=1
-start_game=False
-game_mask=False
+# game settings
+level_selected = 1
+start_game = False
+game_mask = False
 
 
-while True: 
-    breakflag=False
-    for ev in pygame.event.get(): 
-          
-        if ev.type == pygame.QUIT: 
-            pygame.quit() 
-              
-        #checks if a mouse is clicked 
-        if ev.type == pygame.MOUSEBUTTONDOWN: 
-              
-            #if the mouse is clicked on the 
-            # button the game is terminated 
-            if x_quit_btn<= mouse[0] <= x_quit_btn+ width_quit_btn and y_quit_btn <= mouse[1] <= y_quit_btn+height_quit_btn: 
-                pygame.quit() 
-                breakflag=True
+while True:
+    breakflag = False
+    for ev in pygame.event.get():
+
+        if ev.type == pygame.QUIT:
+            pygame.quit()
+
+        # checks if a mouse is clicked
+        if ev.type == pygame.MOUSEBUTTONDOWN:
+
+            # if the mouse is clicked on the
+            # button the game is terminated
+            if x_quit_btn <= mouse[0] <= x_quit_btn + width_quit_btn and y_quit_btn <= mouse[1] <= y_quit_btn+height_quit_btn:
+                pygame.quit()
+                breakflag = True
                 break
-            elif x_start_btn <= mouse[0] <= x_start_btn+width_start_btn and y_start_btn <= mouse[1] <= y_start_btn+height_start_btn: 
-                start_game=True
-                #level_selected=1
-                breakflag=True
+            elif x_start_btn <= mouse[0] <= x_start_btn+width_start_btn and y_start_btn <= mouse[1] <= y_start_btn+height_start_btn:
+                start_game = True
+                # level_selected=1
+                breakflag = True
                 break
-            elif x_tut_btn <= mouse[0] <= x_tut_btn+width_tut_btn and y_tut_btn <= mouse[1] <= y_tut_btn+height_tut_btn: 
-                start_game=True
-                level_selected=10
-                breakflag=True
+            elif x_tut_btn <= mouse[0] <= x_tut_btn+width_tut_btn and y_tut_btn <= mouse[1] <= y_tut_btn+height_tut_btn:
+                start_game = True
+                level_selected = 10
+                breakflag = True
                 break
-    if breakflag==True:
+    if breakflag == True:
         break
-                  
-    # fills the screen with a color 
-    screen.fill((200,200,    0)) 
-      
-    # stores the (x,y) coordinates into 
-    # the variable as a tuple 
-    mouse = pygame.mouse.get_pos() 
-      
-    # if mouse is hovered on a button it 
-    # changes to lighter shade 
-    #start btn
-    if x_start_btn <= mouse[0] <= x_start_btn+width_start_btn and y_start_btn<= mouse[1] <= y_start_btn+height_start_btn: 
-        pygame.draw.rect(screen,color_light,[x_start_btn,y_start_btn,width_start_btn,height_start_btn]) 
-    #tut_btn
-    elif x_tut_btn <= mouse[0] <= x_tut_btn+width_tut_btn and y_tut_btn<= mouse[1] <= y_tut_btn+height_tut_btn: 
-        pygame.draw.rect(screen,color_light,[x_tut_btn,y_tut_btn,width_tut_btn,height_tut_btn]) 
-    #quit_btn
-    elif x_quit_btn <= mouse[0] <= x_quit_btn+width_quit_btn and y_quit_btn<= mouse[1] <= y_quit_btn+height_quit_btn : 
-        pygame.draw.rect(screen,color_light,[x_quit_btn,y_quit_btn,width_quit_btn,height_quit_btn]) 
-    #not hovered above
+
+    # fills the screen with a color
+    screen.fill((200, 200,    0))
+
+    # stores the (x,y) coordinates into
+    # the variable as a tuple
+    mouse = pygame.mouse.get_pos()
+
+    # if mouse is hovered on a button it
+    # changes to lighter shade
+    # start btn
+    if x_start_btn <= mouse[0] <= x_start_btn+width_start_btn and y_start_btn <= mouse[1] <= y_start_btn+height_start_btn:
+        pygame.draw.rect(screen, color_light, [
+                         x_start_btn, y_start_btn, width_start_btn, height_start_btn])
+    # tut_btn
+    elif x_tut_btn <= mouse[0] <= x_tut_btn+width_tut_btn and y_tut_btn <= mouse[1] <= y_tut_btn+height_tut_btn:
+        pygame.draw.rect(screen, color_light, [
+                         x_tut_btn, y_tut_btn, width_tut_btn, height_tut_btn])
+    # quit_btn
+    elif x_quit_btn <= mouse[0] <= x_quit_btn+width_quit_btn and y_quit_btn <= mouse[1] <= y_quit_btn+height_quit_btn:
+        pygame.draw.rect(screen, color_light, [
+                         x_quit_btn, y_quit_btn, width_quit_btn, height_quit_btn])
+    # not hovered above
     else:
-        pygame.draw.rect(screen,color_dark,[x_start_btn,y_start_btn,width_start_btn,height_start_btn]) 
-        pygame.draw.rect(screen,color_dark,[x_tut_btn,y_tut_btn,width_tut_btn,height_tut_btn])
-        pygame.draw.rect(screen,color_dark,[x_quit_btn,y_quit_btn,width_quit_btn,height_quit_btn])
-        #draw title
-        pygame.draw.rect(screen,color_dark,[x_title,y_title,width_title,height_title]) 
-    # superimposing the text onto our button 
-    screen.blit(text_quit , (x_quit_btn+50,y_quit_btn)) 
-    screen.blit(text_start , (x_start_btn+50,y_start_btn)) 
-    screen.blit(text_help , (x_tut_btn+50,y_tut_btn)) 
-    screen.blit(text_title , (x_title+50,y_title)) 
-    # updates the frames of the game 
-    pygame.display.update() 
+        pygame.draw.rect(screen, color_dark, [
+                         x_start_btn, y_start_btn, width_start_btn, height_start_btn])
+        pygame.draw.rect(screen, color_dark, [
+                         x_tut_btn, y_tut_btn, width_tut_btn, height_tut_btn])
+        pygame.draw.rect(screen, color_dark, [
+                         x_quit_btn, y_quit_btn, width_quit_btn, height_quit_btn])
+        # draw title
+        pygame.draw.rect(screen, color_dark, [
+                         x_title, y_title, width_title, height_title])
+    # superimposing the text onto our button
+    screen.blit(text_quit, (x_quit_btn+50, y_quit_btn))
+    screen.blit(text_start, (x_start_btn+50, y_start_btn))
+    screen.blit(text_help, (x_tut_btn+50, y_tut_btn))
+    screen.blit(text_title, (x_title+50, y_title))
+    # updates the frames of the game
+    pygame.display.update()
 
 
 if(start_game):
@@ -135,4 +142,4 @@ if(start_game):
     game = Game(level=level_selected, debug=True)
     game.run_game()
 
-pygame.quit() 
+pygame.quit()
